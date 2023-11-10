e9ac1f1d09d1735c3baa046e74af0862c8474cd8
Alan Milke <amilke@contacrece.com>
2023-11-10 14:03:55 -0600

    Commit Inicial

---
+++ printer_pos/Program.cs
@@ -0,0 +1,42 @@
+﻿using ESC_POS_USB_NET.Printer;
+
+namespace printer_twitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String printerName = "POS58 Printer";
+            String text = "You can print with this printer!";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "--printer":
+                        if (args.Length > i + 1 && args[i + 1].Length > 0)
+                        {
+                            printerName = args[i + 1];
+                            i++;
+                            break;
+                        } // Missing argument?
+                        break;
+                    default:
+                        text = args[i];
+                        break;
+                }
+            }
+
+            Printer printer = new Printer(printerName, "ascii"); 
+
+            printer.Append(text);
+            printer.FullPaperCut();
+            printer.PrintDocument();
+        }
+
+        static void Help()
+        {
+            Console.WriteLine("Arguments: --printer \"PrinterName\" - Uses the device named \"PrinterName\" to print (Default:POS58 Printer)");
+        }
+    }
+}
---
+++ printer_pos/README.md
@@ -0,0 +1,7 @@
+# Printer POS
+
+Este es un pequeño programa de consola para imprimir por medio de una impresora de tickets (china?) enviando el nombre de la impresora y el texto a imprimir.
+
+## Overview
+
+Básicamente llamas al programa compilado (printer_pos.exe) y envías dos parametros --printer "<impresora>" "mensaje a imprimir"
